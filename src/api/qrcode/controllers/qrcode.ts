7e96c43c2ab49bed7636bedfa6346db3dfadb455
Davel <luisdavel90@outlook.com>
2022-12-20 22:55:09 -0300

    Strapi

---
+++ src/api/qrcode/controllers/qrcode.ts
@@ -0,0 +1,7 @@
+/**
+ * qrcode controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::qrcode.qrcode');
---
+++ src/api/qrcode/routes/qrcode.ts
@@ -0,0 +1,7 @@
+/**
+ * qrcode router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::qrcode.qrcode');
